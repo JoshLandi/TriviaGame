eff2932ac923deb93fc104269e1932f3a4455071
Josh L <joshtlandi@gmail.com>
2018-08-25 10:00:15 -0700

    Make it pretty (begin)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,6 +35,7 @@ var triviaGame = {
     counter: 10,
     correct: 0,
     incorrect: 0,
+    unanswered: 0,
 
     countdown: function(){
 
@@ -50,13 +51,14 @@ var triviaGame = {
     },
 
     loadQuestion: function(){
-        
+
         timer = setInterval(triviaGame.countdown, 1000);
 
-        $("#wrapper2").html("<h2>" + questions[triviaGame.currentQuestion].question + "</h2>");
+        $("#wrapper2").html("<h2>Maize Remaining: <span id='counter'>10</span> seconds</h2>");
+        $("#wrapper2").append("<h2>" + questions[triviaGame.currentQuestion].question + "</h2>");
 
         for (var i = 0; i < questions[triviaGame.currentQuestion].answers.length; i++) {
-            $("#wrapper2").append("<button class='answer-button' id='button- " + i + " data-name='" + questions[triviaGame.currentQuestion].answers[i] + "'>" + questions[triviaGame.currentQuestion].answers[i]+"</button>");
+            $("#wrapper2").append("<button class='answer-button' id='button- " + i + "' data-name='" + questions[triviaGame.currentQuestion].answers[i] + "'>" + questions[triviaGame.currentQuestion].answers[i]+"</button>");
             
         }
         
@@ -64,38 +66,129 @@ var triviaGame = {
 
     nextQuestion: function(){
 
+        triviaGame.counter = 10;
+
+        $("#counter").html(triviaGame.counter);
+
+        triviaGame.currentQuestion++;
+
+        triviaGame.loadQuestion();
+
     },
 
     timeUp: function(){
+
+        clearInterval(timer);
+        triviaGame.unanswered++;
+        $("#wrapper2").html("<h2>OUT OF MAIZE</h2>");
+        $("#wrapper2").append("<h3>The Correct Answer Was: " + questions[triviaGame.currentQuestion].correctAns + "</h3>");
+
+        if (triviaGame.currentQuestion == questions.length -1){
+            
+            setTimeout(triviaGame.results, 3*1000);
+
+        } else {
+
+            setTimeout(triviaGame.nextQuestion, 3*1000);
+        }
 
     },
 
     results: function(){
 
+        clearInterval(timer);
+        $("#wrapper2").html("<h1>Maize Navigated</h1>");
+        $("#wrapper2").append("<h1>Correct: " + triviaGame.correct + "</h1>");
+        $("#wrapper2").append("<h1>Incorrect: " + triviaGame.incorrect + "</h1>");
+        $("#wrapper2").append("<h1>Unanswered: " + triviaGame.unanswered + "</h1>")
+
+        $("#wrapper2").append("<button id='reset'>CANT GET ENOUGH</button>")
     },
 
-    clicked: function(){
+    clicked: function(event){
+
+        clearInterval(timer);
+
+        if($(event.target).data("name") == questions[triviaGame.currentQuestion].correctAns){
+            
+            triviaGame.answeredCorrect();
+            
+           
+        } else {
+
+            triviaGame.answeredIncorrect();
+            
+        }
 
     },
 
     answeredCorrect: function(){
+        
+        console.log("Correct");
+        clearInterval(timer);
 
+        triviaGame.correct++;
+        $("#wrapper2").html("<h2>YOU'RE A MAIZE MASTER!</h2>");
+
+        if (triviaGame.currentQuestion == questions.length -1){
+            
+            setTimeout(triviaGame.results, 3*1000);
+
+        } else {
+
+            setTimeout(triviaGame.nextQuestion, 3*1000);
+        }
     },
 
     answeredIncorrect: function(){
+        
+        console.log("Incorrect");
+        clearInterval(timer);
 
+        triviaGame.incorrect++;
+        $("#wrapper2").html("<h2>It's aMAIZEing how dumb you are (about maize).</h2>");
+        $("#wrapper2").append("<h3>The Correct Answer Was: " + questions[triviaGame.currentQuestion].correctAns + "</h3>");
+
+        if (triviaGame.currentQuestion == questions.length -1){
+            
+            setTimeout(triviaGame.results, 3*1000);
+
+        } else {
+
+            setTimeout(triviaGame.nextQuestion, 3*1000);
+        }
     },
 
     reset: function(){
 
+        triviaGame.currentQuestion = 0;
+        triviaGame.counter = 0;
+        triviaGame.correct = 0;
+        triviaGame.incorrect = 0;
+        triviaGame.unanswered = 0;
+        triviaGame.loadQuestion();
     },
 
 
 }
 
-//Functions
+//Click Events
 
 $("#Begin").on("click", function(){
+
     $("#Begin").remove();
     triviaGame.loadQuestion();
-})
+
+});
+
+$(document).on("click", ".answer-button", function(event){
+
+    triviaGame.clicked(event);
+
+});
+
+$(document).on("click", "#reset", function(){
+
+    triviaGame.reset();
+
+});
